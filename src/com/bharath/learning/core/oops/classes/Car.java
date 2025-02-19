788d0bf997c355faee51bc7fbbd0e028c02e3d4c
Bharath Ashok <iambharath.ashoka@gmail.com>
2025-02-19 06:48:13 +0530

    Multiple Inheritance Constructor and Classes Examples

--- src/com/bharath/learning/core/oops/classes/Car.java
+++ src/com/bharath/learning/core/oops/classes/Car.java
@@ -1,4 +1,84 @@
 package com.bharath.learning.core.oops.classes;
 
 public class Car {
+
+    // Attributes
+
+    // Private files (Encapsulation)
+    private String brand; // No accessible outside this class
+    private int speed;
+
+
+    protected String steeringWheel;
+
+
+    // Constructor
+    public Car (String brand, int speed) {
+        this.brand = brand;
+        this.speed = speed;
+    }
+
+
+    // Methods or Behaviours
+
+
+    public void accelerate(int increase) {
+        speed += increase;
+        System.out.println(this.brand   + " acceralted to "+ this.speed + "km /h");
+
+    }
+
+
+    public void brake(int decrease) {
+        speed -= decrease;
+        System.out.println(this.brand   + " slowed down to "+ this.speed + "km /h");
+
+    }
+
+}
+
+class Benz extends Car {
+
+    public Benz(String brand, int speed) {
+        super(brand, speed);
+    }
+
+
+}
+
+
+class Main {
+
+    public static void main(String[] args) {
+        int counter = 10;
+
+        // Creating object of the Car class
+
+        Car benz = new Car("Benz", 100);
+        Car bmw = new Car("BMW", 130);
+        Car toyota = new Car("Toyota", 90);
+
+
+        // Using the Objects
+
+        // Calling the Behaviours of Object
+
+
+        bmw.accelerate(49);
+
+        benz.brake(10);
+
+
+
+        Benz benz1 = new Benz("Benz",120);
+
+        benz1.accelerate(90);
+        benz1.brake(10);
+
+
+
+
+
+
+    }
 }
--- src/com/bharath/learning/core/oops/constructors/ZeroArgumentConstructorExample.java
+++ src/com/bharath/learning/core/oops/constructors/ZeroArgumentConstructorExample.java
@@ -1,4 +1,29 @@
 package com.bharath.learning.core.oops.constructors;
 
+class Student {
+
+    private String name;
+    private int age;
+
+    // Zero Argument Constructor - Provided by Programmer
+    public Student() {
+
+    }
+
+
+    public void display() {
+        System.out.println("Name:: "+ name + "  ,Age:: "+ age);
+    }
+
+}
+
+
+
 public class ZeroArgumentConstructorExample {
+
+    public static void main(String[] args) {
+        Student student1  = new Student();
+        student1.display();
+
+    }
 }
