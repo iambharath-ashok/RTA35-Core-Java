7600239dbd169cd164770225ed2dfd1827b915bb
Bharath Ashok <iambharath.ashoka@gmail.com>
2025-02-26 07:24:26 +0530

    Practicle Example on OOPS

---
+++ src/com/bharath/learning/core/oops/practicleexample/BankAccountEncapsulationExample.java
@@ -0,0 +1,25 @@
+package com.bharath.learning.core.oops.practicleexample;
+
+public class BankAccountEncapsulationExample {
+    private int accountNumber;
+    private String accountHolderName;
+    private double balance;
+
+    public int getAccountNumber() {
+        return this.accountNumber;
+    }
+
+    public void setAccountNumber(int accountNumber) {
+        this.accountNumber = accountNumber;
+
+    }
+
+    public String getAccountHolderName() {
+        return this.accountHolderName;
+    }
+
+    public void setAccountHolderName(String accountHolderName) {
+
+        this.accountHolderName = accountHolderName;
+    }
+}
