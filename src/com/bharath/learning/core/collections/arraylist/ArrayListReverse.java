2dcbb05c976027558c9018b6f668aba9943d21fb
Bharath Ashok <iambharath.ashoka@gmail.com>
2025-03-25 07:52:15 +0530

    ArrayList Reverse and LinkedList Basic Methods

---
+++ src/com/bharath/learning/core/collections/arraylist/ArrayListReverse.java
@@ -0,0 +1,22 @@
+package com.bharath.learning.core.collections.arraylist;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class ArrayListReverse {
+
+    public static void main(String[] args) {
+
+        List<String> fruits = new ArrayList<>();
+
+        fruits.add("Apple");
+        fruits.add("Banana");
+        fruits.add("Orange");
+        Collections.reverse(fruits);
+
+
+        System.out.println("Fruits list after reversing: " + fruits);
+
+    }
+}
